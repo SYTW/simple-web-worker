31aa6733376f3848850d1e2d3ec019f1799ee752
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2019-10-14 09:47:59 +0100

    now click button to have the result

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
-var first = document.querySelector('#number1');
-var second = document.querySelector('#number2');
+let first = document.querySelector('#number1');
+let second = document.querySelector('#number2');
+let compute = document.querySelector('#click');
 
-var result = document.querySelector('.result');
+let result = document.querySelector('.result');
 
 if (window.Worker) { //check if Browser supports the Worker api.
-	// Requires script name as input
-	var myWorker = new Worker("worker.js");
+  // Requires script name as input
+  let myWorker = new Worker("worker.js");
 
-	first.onchange = function() {
-	  myWorker.postMessage([first.value,second.value]); //sending message as array to the worker
-	  console.log('Message posted to worker '+first.value+', '+second.value);
-	};
+  compute.onclick = function() {
+    console.log('Message posted to worker '+first.value+', '+second.value);
+    myWorker.postMessage([Number(first.value), Number(second.value)]); //sending message as array to the worker
+  };
 
-	second.onchange = function() {
-	  myWorker.postMessage([first.value,second.value]);
-	  console.log('Message posted to worker '+first.value+', '+second.value);
-	};
-
-	myWorker.onmessage = function(e) {
+  myWorker.onmessage = function(e) {
     console.log(e);
-		result.textContent = e.data;
-		console.log('Message received from worker '+e.data);
-	};
+    result.textContent = e.data;
+    console.log('Message received from worker '+e.data);
+  };
 }
